0c86beb74e9ba9a87ce5990f009a49d2807e4660
ArtBas <art-basalaev@yandex.ru>
2021-11-24 21:09:37 +0700

    исправлена ошибка в сортировке

--- Web/Arrays/scripts/script.js
+++ Web/Arrays/scripts/script.js
@@ -2,18 +2,22 @@
     var array = [12, 1, 2, 13, 14, 10, 8, 4, 3, 15, 6, 9, 7, 5, 11];
 
     array.sort(function (e1, e2) {
-        return e1 - e2;
+        return e2 - e1;
     });
 
     console.log(array.join(", "));
-    console.log(array.slice(0, 5).join(", "));
-    console.log(array.slice(array.length - 5, array.length).join(", "));
+
+    var firstFiveNumbersSubArray = array.slice(0, 5);
+    console.log(firstFiveNumbersSubArray.join(", "));
+
+    var lastFiveNumbersSubArray = array.slice(-5);
+    console.log(lastFiveNumbersSubArray.join(", "));
 
     // 1 вариант
-    console.log((array.filter(function (e) {
+    console.log(array.filter(function (e) {
         return e % 2 === 0;
-    })).reduce(function (sum, e) {
-        return sum += e;
+    }).reduce(function (sum, e) {
+        return sum + e;
     }, 0));
 
     // 2 вариант
@@ -22,22 +26,22 @@
             return sum;
         }
 
-        return sum += e;
+        return sum + e;
     }, 0));
 })();
 
 (function () {
-    var array1 = [];
+    var array = [];
 
     for (var i = 1; i <= 100; i++) {
-        array1.push(i);
+        array.push(i);
     }
 
-    var array2 = array1.filter(function (e) {
+    var evenNumbersSquares = array.filter(function (e) {
         return e % 2 === 0;
     }).map(function (e) {
         return e * e;
     });
 
-    console.log(array2.join(", "));
+    console.log(evenNumbersSquares.join(", "));
 })();
