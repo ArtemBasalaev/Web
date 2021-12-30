8adb77a67967efd9c0c85340392131b3fa253f1c
ArtBas <art-basalaev@yandex.ru>
2021-12-30 22:52:41 +0700

    добавлена валидация формы

--- Web/TodoList_jQuery/script.js
+++ Web/TodoList_jQuery/script.js
@@ -2,24 +2,27 @@ $(function () {
     var todoList = $("#todo-list");
     var newTodoActivity = $("#new-todo-activity");
     var newTodoButton = $("#new-todo-button");
+    var newTodoForm = $("#todo-form");
 
-    newTodoButton.click(function (event) {
+    newTodoButton.click(function () {
+        newTodoForm.addClass("was-validated");
+
         var newActivityText = newTodoActivity.val().trim();
 
         if (newActivityText.length === 0) {
             return;
         }
 
-        event.preventDefault();
-
         var todoListItem = $("<li>");
-        todoListItem.addClass("list-group-item");
+        todoListItem.addClass("list-group-item ps-0");
 
         setViewMode();
 
         todoList.append(todoListItem);
 
         newTodoActivity.val("");
+
+        newTodoForm.removeClass("was-validated");
 
         function setViewMode() {
             todoListItem.html("<span class='list-item-text'></span><div class='mt-2 mb-2'>\
@@ -38,23 +41,25 @@ $(function () {
         }
 
         function setEditMode() {
-            todoListItem.html("<form class='g-3'><input type='text' class='form-control new-activity-text' required><div class='mt-2 mb-2'>\
-                               <button type='submit' class='btn btn-success btn-sm save-button'>Сохранить</button>\
-                               <button type='button' class='btn btn-danger btn-sm cancel-button'>Отменить</button>\
-                               </div></form>");
+            todoListItem.html("<form class='g-3 needs-validation' novalidate>\
+                               <input type='text' class='form-control new-activity-text' required>\
+                               <div class='invalid-feedback'>*заполните поле</div><div class='mt-2 mb-2'>\
+                               <button type='button' class='btn btn-success btn-sm save-button'>Сохранить</button>\
+                               <button type='button' class='btn btn-danger btn-sm cancel-button'>Отменить</button></form>");
 
             var updateActivityInput = todoListItem.find(".new-activity-text");
+            var listItemForm = todoListItem.find(".needs-validation");
 
             updateActivityInput.val(newActivityText);
 
-            todoListItem.find(".save-button").click(function (event) {
+            todoListItem.find(".save-button").click(function () {
+                listItemForm.addClass("was-validated");
+
                 var updateActivityText = updateActivityInput.val().trim();
 
                 if (updateActivityText.length === 0) {
                     return;
                 }
-
-                event.preventDefault();
 
                 newActivityText = updateActivityText;
 
