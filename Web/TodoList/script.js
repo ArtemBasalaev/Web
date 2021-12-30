7051b159c54a17221b0244b7fefe8c0c18d05a62
ArtBas <art-basalaev@yandex.ru>
2021-12-30 22:11:39 +0700

    добавлена валидация форм

--- Web/TodoList/script.js
+++ Web/TodoList/script.js
@@ -2,24 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
     var todoList = document.getElementById("todo-list");
     var newTodoActivity = document.getElementById("new-todo-activity");
     var newTodoButton = document.getElementById("new-todo-button");
+    var newTodoForm = document.getElementById("todo-form");
 
-    newTodoButton.addEventListener("click", function (event) {
+    newTodoButton.addEventListener("click", function () {
+        newTodoForm.classList.add("was-validated");
+
         var newActivityText = newTodoActivity.value.trim();
 
         if (newActivityText.length === 0) {
             return;
         }
 
-        event.preventDefault();
-
         var todoListItem = document.createElement("li");
-        todoListItem.classList.add("list-group-item");
+        todoListItem.classList.add("list-group-item", "ps-0");
 
         setViewMode();
 
         todoList.appendChild(todoListItem);
 
         newTodoActivity.value = "";
+
+        newTodoForm.classList.remove("was-validated");
 
         function setViewMode() {
             todoListItem.innerHTML = "<span class='list-item-text'></span><div class='mt-2 mb-2'>\
@@ -38,23 +41,25 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         function setEditMode() {
-            todoListItem.innerHTML = "<form class='g-3'><input type='text' class='form-control new-activity-text' required><div class='mt-2 mb-2'>\
-                                      <button type='submit' class='btn btn-success btn-sm save-button'>Сохранить</button>\
-                                      <button type='button' class='btn btn-danger btn-sm cancel-button'>Отменить</button>\
-                                      </div></form>";
+            todoListItem.innerHTML = "<form class='g-3 needs-validation' novalidate>\
+                                      <input type='text' class='form-control new-activity-text' required>\
+                                      <div class='invalid-feedback'>*заполните поле</div><div class='mt-2 mb-2'>\
+                                      <button type='button' class='btn btn-success btn-sm save-button'>Сохранить</button>\
+                                      <button type='button' class='btn btn-danger btn-sm cancel-button'>Отменить</button></form>";
 
             var updateActivityInput = todoListItem.querySelector(".new-activity-text");
+            var listItemForm = todoListItem.querySelector(".needs-validation");
 
             updateActivityInput.value = newActivityText;
 
-            todoListItem.querySelector(".save-button").addEventListener("click", function (event) {
+            todoListItem.querySelector(".save-button").addEventListener("click", function () {
+                listItemForm.classList.add("was-validated");
+
                 var updateActivityText = updateActivityInput.value.trim();
 
                 if (updateActivityText.length === 0) {
                     return;
                 }
-
-                event.preventDefault();
 
                 newActivityText = updateActivityText;
 
