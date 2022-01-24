0eded43c905b572f93dea3139d0c840f52f56d8c
ArtBas <art-basalaev@yandex.ru>
2022-01-24 20:08:29 +0700

    добавлены классы для взаимодействия с сервером

---
+++ Web/PhoneBookWebpack/src/javascripts/ApiUrls.js
@@ -0,0 +1,11 @@
+export default class ApiUrls {
+    constructor(getContactsUrl, createContactUrl, deleteContactUrl, deleteContactsUrl) {
+        this.getContactsUrl = getContactsUrl;
+
+        this.createContactUrl = createContactUrl;
+
+        this.deleteContactUrl = deleteContactUrl;
+
+        this.deleteContactsUrl = deleteContactsUrl;
+    }
+};
---
+++ Web/PhoneBookWebpack/src/javascripts/RequestMethods.js
@@ -0,0 +1,38 @@
+import $ from "jquery";
+
+export default class RequestMethods {
+    constructor(apiUrls) {
+        this.apiUrls = apiUrls;
+    }
+
+    get(url, data) {
+        return $.get({
+            url,
+            data
+        });
+    }
+
+    post(url, data) {
+        return $.post({
+            url,
+            data: JSON.stringify(data),
+            contentType: "application/json"
+        });
+    }
+
+    getContacts(term){
+        return this.get(this.apiUrls.getContactsUrl, term);
+    }
+
+    createContact(contact){
+        return this.post(this.apiUrls.createContactUrl, contact);
+    }
+
+    deleteContact(contactId){
+        return this.post(this.apiUrls.deleteContactUrl, contactId);
+    }
+
+    deleteContacts(contactsId){
+        return this.post(this.apiUrls.deleteContactsUrl, contactsId);
+    }
+};
